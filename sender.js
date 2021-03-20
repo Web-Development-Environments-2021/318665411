eb5c1518d7fcb8855594d7232e851aa1189bbe5f
roi <roirein@post.bgu.ac.il>
2021-03-21 00:35:12 +0200

    menu,about,photos,playlist

--- sender.js
+++ sender.js
@@ -4,13 +4,10 @@ function sendEmail() {
     alert("Email sent successfully")
 }
 
-function addToText(){
+const addToText= ()=>{
         let text = document.getElementById('msg').value
         const imogi = document.getElementById('selection')
         let chosen = imogi.options[imogi.selectedIndex].value
-        if(chosen){
-            document.getElementById('msg').value = text + chosen
-        }
-        imogi.value=imogi.options[0]
+        document.getElementById('msg').value = text + chosen
 
 }
