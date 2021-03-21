b8585bcaabf6f2ad6e768688579c1f49bee19f3f
roi <roirein@post.bgu.ac.il>
2021-03-22 01:03:10 +0200

    contact,qr,like

--- sender.js
+++ sender.js
@@ -1,13 +1,29 @@
 function sendEmail() {
-    document.getElementById('msg').value= ""
-    document.getElementById('mail').value= ""
-    alert("Email sent successfully")
+    const first_name = document.getElementById('fname').value
+    const last_name = document.getElementById('lname').value
+    const message = document.getElementById('message').value
+    const from_mail = document.getElementById('mail').value
+    let temp = {
+        from_name : first_name + " " + last_name,
+        to_name : "Roi",
+        message : message,
+        from_email: from_mail
+    }
+
+    emailjs.send('gmail','template_9bxpfdt',temp).then(function (response){
+        alert("email sent")
+    })
+
 }
 
-const addToText= ()=>{
-        let text = document.getElementById('msg').value
-        const imogi = document.getElementById('selection')
-        let chosen = imogi.options[imogi.selectedIndex].value
-        document.getElementById('msg').value = text + chosen
-
+const addToText= ()=> {
+    let text = document.getElementById('message').value
+    const imogi = document.getElementById('selection')
+    let chosen = imogi.options[imogi.selectedIndex].text
+    if (chosen) {
+        document.getElementById('message').value = text + chosen
+    }
+    imogi.options[imogi.selectedIndex].text = imogi.options[0].text
 }
+
+
