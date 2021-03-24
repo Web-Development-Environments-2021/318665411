22792542bd8081411456bee38fd62b4e4009a831
roi <roirein@post.bgu.ac.il>
2021-03-24 16:44:26 +0200

    contact, styling and fixes

--- sender.js
+++ sender.js
@@ -1,24 +1,3 @@
-function sendEmail() {
-    const first_name = document.getElementById('fname').value
-    const last_name = document.getElementById('lname').value
-    const message = document.getElementById('message').value
-    const from_mail = document.getElementById('mail').value
-    let temp = {
-        from_name : first_name + " " + last_name,
-        to_name : "Roi",
-        message : message,
-        from_email: from_mail
-    }
-
-    emailjs.send('gmail','template_9bxpfdt',temp).then(function (response){
-        alert("email sent")
-    })
-    document.getElementById('fname').value=''
-    document.getElementById('lname').value=''
-    document.getElementById('message').value=''
-    document.getElementById('mail').value=''
-}
-
 const addToText= ()=> {
     let text = document.getElementById('message').value
     const imogi = document.getElementById('selection')
