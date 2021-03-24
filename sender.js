b7df8f9966ca5fdbf04b3324e79b94f5e329ab7e
roi <roirein@post.bgu.ac.il>
2021-03-24 14:21:24 +0200

    styling and fixes

--- sender.js
+++ sender.js
@@ -13,14 +13,17 @@ function sendEmail() {
     emailjs.send('gmail','template_9bxpfdt',temp).then(function (response){
         alert("email sent")
     })
-
+    document.getElementById('fname').value=''
+    document.getElementById('lname').value=''
+    document.getElementById('message').value=''
+    document.getElementById('mail').value=''
 }
 
 const addToText= ()=> {
     let text = document.getElementById('message').value
     const imogi = document.getElementById('selection')
     let chosen = imogi.options[imogi.selectedIndex].text
-    if (chosen) {
+    if (chosen !== imogi.options[0].text) {
         document.getElementById('message').value = text + chosen
     }
     imogi.selectedIndex = imogi.options[0]
